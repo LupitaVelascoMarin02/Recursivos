6282405ea2449e5579a3a139839b45c08e6d2022
LupitaVelascoMarin02 <51174141+LupitaVelascoMarin02@users.noreply.github.com>
2020-11-10 16:17:53 -0600

    Add files via upload

--- UnoRecursivo/src/unoRecursivo/Principal.java
+++ UnoRecursivo/src/unoRecursivo/Principal.java
@@ -17,15 +17,24 @@ public static void main(String[] args) {
 
 	static void metodoR(ArrayList<Integer> nums, int fin, ArrayList<Integer> por) {
         int suma = 0;
+        
         for (int x: por) suma += x;
+        
         if (suma == fin)
-             System.out.println("suma("+Arrays.toString(por.toArray())+")="+fin);
+        	
+             System.out.println("Las combinaciones donde la suma sea igual a "+ fin +" son :"+Arrays.toString(por.toArray()));
+        
         if (suma >= fin)
+        	
              return;
+        
         for(int i=0;i<nums.size();i++) {
+        	
         	ArrayList<Integer> sobrante = new ArrayList<Integer>();
         	int numero = nums.get(i);
+        	
         	for (int j=i+1; j<nums.size();j++)
+        		
         		sobrante.add(nums.get(j));
         	ArrayList<Integer> porapoyo = new ArrayList<Integer>(por);
         	porapoyo.add(numero);
